53de967984660098861a18ddd29447e93d6d5aaf
Gaurav Rattan <gauravrattan76@gmail.com>
2020-03-18 16:33:18 +0530

    First commit

---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Gaurav Rattan!!</h1>
+// }
+
+const Greet = () => <h1>Hello Gaurav Rattan how are you buddy!!!</h1>
+
+export default Greet
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React, { Component} from 'react'
+
+class Welcome extends Component{
+    render(){
+        return <h1>Hello from the react component!!!</h1>
+    }
+}
+
+export default Welcome
