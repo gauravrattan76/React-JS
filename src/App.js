0c9e1ecdf7a3bdf1bf1eeedc90d0f442337200a9
Gaurav Rattan <gauravrattan76@gmail.com>
2020-03-18 22:04:40 +0530

    state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Gaurav" heroName="superman">
+      <Message></Message>
+      {/* <Greet name="Gaurav" heroName="superman">
         <h4>This is the super man kingdom</h4>
       </Greet>
       <Greet name="Jack" heroName="Batman">
@@ -17,9 +19,10 @@ function App() {
       <Greet name="Danny" heroName="Ironman"></Greet>
       <Welcome name="Gaurav" heroName="superman"></Welcome>
       <Welcome name="Jack" heroName="Batman">
-      <h4>This is the bat man kingdom</h4>
+        <h4>This is the bat man kingdom</h4>
       </Welcome>
-      <Hello></Hello>
+      <Hello></Hello> */}
+
     </div>
   );
 }
