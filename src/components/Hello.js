dc0b163c32bdeb8991fb6d41f377c6ab398b57dd
Gaurav Rattan <gauravrattan76@gmail.com>
2020-03-18 20:41:44 +0530

    JSX Commit

---
+++ src/components/Hello.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hello = () => {
+    //JSX Version
+    //Each JSX is a syntactic sugar for calling the React.createElement
+    //JSX internally converts into React.createElement which uses the react library
+    // return(
+    //     <div>
+    //         <h1>This is JSX Component!!</h1>
+    //     </div>
+    // )
+    return React.createElement('div',
+    {id:'content',className:'mainClass'},
+   React.createElement('h1',null,'How are you buddy!!')
+    )
+}
+
+export default Hello
