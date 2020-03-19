d516ac061e073beb90e521bf1c6c96c153a3aba4
Gaurav Rattan <gauravrattan76@gmail.com>
2020-03-19 13:51:46 +0530

    binding event handlers

---
+++ src/components/EventBind.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+ class EventBind extends Component {
+     constructor(props) {
+         super(props)
+     
+         this.state = {
+              message:"Hello"
+         }
+         //best approcah to bind in the class constructor
+         this.eventHandler =this.eventHandler.bind(this)
+     }
+     //this is undefined in event handler so event binding is required in react
+     eventHandler(){
+         this.setState({
+             message: "Good bye"
+         })
+        console.log(this);
+     }
+     
+    render() {
+        return (
+            <div>
+                <div>{this.state.message}</div>
+                {/* <button onClick={this.eventHandler.bind(this)}>Click</button> */}
+                {/* <button onClick={() => this.eventHandler()}>Click</button> */}
+                <button onClick={this.eventHandler}>Click</button> 
+            </div>
+        )
+    }
+}
+
+export default EventBind
